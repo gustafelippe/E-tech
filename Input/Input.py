49ab58d8047a2fb09b402c428eb974ab772698d1
gustafelippe <gustavo.gugu77761@gmail.com>
2025-11-25 21:03:57 -0400

    add altura-float

--- Input/Input.py
+++ Input/Input.py
@@ -3,3 +3,6 @@ print(f"Olá, {nome}! Bem-vindo(a) ao Python")
 
 idade = int(input("Digite sua idade: "))
 print(f"Daqui a 5 anos, você terá {idade + 5} anos.")
+
+altura = float(input("Digite sua altura em metros: "))
+print(f"Sua altura é de {altura} metros, ou {altura + 100} centimetros.")
